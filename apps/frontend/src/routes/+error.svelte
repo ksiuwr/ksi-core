df95b2f16e1af548de48ec4869b049cbaf530c9a
crqch <48185508+crqch@users.noreply.github.com>
2025-11-15 18:38:49 +0100

    feat(ui): add error route

---
+++ apps/frontend/src/routes/+error.svelte
@@ -0,0 +1,19 @@
+<script>
+	import Wrapper from '../components/Wrapper.svelte';
+</script>
+
+<Wrapper
+	containerClassName="markdown-file min-h-screen"
+	textClassName="text-error font-bold"
+	name="ERROR.md"
+>
+	<h1 class="md:mt-20 mt-10">Ojoj, a co tu się zadziało?</h1>
+	<p>
+		Albo coś się zepsuło, albo ktoś szukał szczęścia na naszej stronie. Zapewniamy, że jedyne
+		szczęście kryje się w <b>pokoju 24</b> na <b>Instytucie Informatyki</b>!
+	</p>
+
+	<h2>
+		Zapraszamy w wolnym czasie, a teraz <a href="/">kliknij tutaj</a>, aby wrócić do domu.
+	</h2>
+</Wrapper>
