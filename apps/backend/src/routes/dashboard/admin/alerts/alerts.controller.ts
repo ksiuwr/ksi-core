b64df1a191ea6a607529147ab76fe457e671b7f7
crqch <48185508+crqch@users.noreply.github.com>
2025-11-17 01:02:58 +0100

    feat: add CRUD endpoints for alerts, stage frontend

--- apps/backend/src/routes/dashboard/admin/alerts/alerts.controller.ts
+++ apps/backend/src/routes/dashboard/admin/alerts/alerts.controller.ts
@@ -1,19 +1,73 @@
 import { alert } from "@ksi-core/backend/db";
 import { createElysia } from "@ksi-core/backend/lib/createElysia";
-import { eq } from "drizzle-orm";
-import { t } from "elysia";
+import { AlertBodyType } from "@ksi-core/backend/routes/dashboard/admin/alerts/alerts.type";
+import { _cuid2 } from "better-auth";
+import { count, desc, eq } from "drizzle-orm";
+import { status, t } from "elysia";
 
 export default createElysia({
     prefix: '/alerts',
     protectedRoute: true
 })
     .get("/", async ({ db, query }) => {
-        db.query.alert.findMany({
-            ...query
+        const results = await db.query.alert.findMany({
+            offset: query.page * 20,
+            limit: 20,
+            orderBy: desc(alert.startDate)
         })
+        const count = await db.$count(alert);
+
+        return {
+            results,
+            pages: Math.ceil(count / 20)
+        }
     }, {
         query: t.Object({
-            limit: t.Number(),
-            offset: t.Number()
+            page: t.Numeric()
+        })
+    })
+    .post("/", async ({ db, body, user }) => {
+        await db.insert(alert).values({
+            ...body,
+            createdBy: user.id
+        })
+
+        return status(200, { code: "CREATED_ALERT" });
+    }, {
+        ...AlertBodyType
+    })
+    .put("/:id", async ({ db, params, body }) => {
+        const total = await db.$count(alert, eq(alert.id, params.id));
+        if (total === 0) return status(404, `Alert ${params.id} was not found!`);
+        await db.update(alert).set({
+            ...body
+        }).where(eq(alert.id, params.id))
+
+        return status(200, {
+            code: "UPDATED_ALERT",
+            code_values: {
+                id: params.id
+            }
+        })
+    }, {
+        ...AlertBodyType,
+        params: t.Object({
+            id: t.String()
+        })
+    })
+    .delete("/:id", async ({ db, params }) => {
+        const total = await db.$count(alert, eq(alert.id, params.id));
+        if (total === 0) return status(404, `Alert ${params.id} was not found!`);
+        await db.delete(alert).where(eq(alert.id, params.id))
+
+        return status(200, {
+            code: "DELETED_ALERT",
+            code_values: {
+                id: params.id
+            }
+        })
+    }, {
+        params: t.Object({
+            id: t.String()
         })
     })
--- apps/frontend/src/routes/dashboard/alerts/+page.svelte
+++ apps/frontend/src/routes/dashboard/alerts/+page.svelte
@@ -1,13 +1,75 @@
-<script>
-	import { Megaphone } from '@lucide/svelte';
+<script lang="ts">
+	import { Megaphone, Plus } from '@lucide/svelte';
 	import Wrapper from '../../../components/Wrapper.svelte';
+	import { api, resolveError } from '$lib/backend';
+	import { toast } from 'svelte-sonner';
+	import ms from 'ms';
+
+	let alerts = $state<
+		NonNullable<Awaited<ReturnType<typeof api.dashboard.admin.alerts.get>>['data']>['results']
+	>([]);
+	let maxPage = $state(0);
+	let currentPage = $state(0);
+
+	let pages = $derived([
+		0,
+		...[currentPage - 1, currentPage, currentPage + 1].filter((p) => p > 1 && p < maxPage),
+		...[maxPage].filter((p) => p !== 0)
+	]);
+
+	let creating = $state(false);
+	let newAlert = $state({
+		title: '',
+		description: '',
+		color: 'neutral',
+		startDate: new Date(),
+		endDate: new Date(new Date().getTime() + ms('7d'))
+	});
+
+	$effect(() => {
+		api.dashboard.admin.alerts
+			.get({
+				query: {
+					page: currentPage
+				}
+			})
+			.then((r) => {
+				if (r.error) {
+					toast.error(resolveError(r.error));
+					return;
+				}
+				alerts = r.data.results;
+				maxPage = r.data.pages;
+			});
+	});
 </script>
 
 {#snippet icon()}
 	<Megaphone class="size-4" />
 {/snippet}
 
+{#snippet pageButton(page: number)}
+	<button
+		class="btn btn-square btn-sm btn-outline"
+		disabled={currentPage === page}
+		onclick={() => (currentPage = page)}>{page + 1}</button
+	>
+{/snippet}
+
 <Wrapper containerClassName="markdown-file" name="dashboard/ALERTS.md" {icon}>
 	<h2>Alerty</h2>
 	Zarządzaj alertem wyświetlanym na górze strony.
 </Wrapper>
+<div class="flex flex-col my-8 gap-4">
+	<button class="btn btn-outline btn-lg rounded-none w-fit self-center">
+		<Plus class="size-4" /> utwórz nowy
+	</button>
+	{#each alerts as alert}
+		<div class="flex flex-col p-2 border border-base-200"></div>
+	{/each}
+	<div class="flex gap-2 self-center">
+		{#each pages as page}
+			{@render pageButton(page)}
+		{/each}
+	</div>
+</div>
