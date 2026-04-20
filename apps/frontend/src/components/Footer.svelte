f9d8b3b45eec771e0baad5618359c2fb857604e0
crqch <48185508+crqch@users.noreply.github.com>
2026-04-21 00:30:33 +0200

    chore: add source code link to footer

--- apps/frontend/src/components/Footer.svelte
+++ apps/frontend/src/components/Footer.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { m } from '$lib/paraglide/messages';
+	import { Github } from '@lucide/svelte';
 </script>
 
 <footer class="mt-auto w-full border-t border-base-content/10 bg-base-100 py-10 text-sm">
@@ -13,9 +14,17 @@
 			</address>
 		</div>
 
-		<div class="flex flex-col gap-1 md:text-right">
+		<div class="flex flex-col gap-1 md:text-right md:items-end">
 			<p class="font-mono font-medium text-primary transition-colors">ksi [at] cs.uni.wroc.pl</p>
 			<span class="text-base-content/40">© KSI {new Date().getFullYear()}</span>
+			<a
+				href="https://github.com/ksiuwr/ksi-core"
+				class="gap-2 text-base-content/40 hover:text-primary"
+				target="_blank"
+			>
+				<Github class="inline w-3 h-3 mr-px" />
+				{m.loud_calm_horse_hide()}
+			</a>
 		</div>
 	</div>
 </footer>
