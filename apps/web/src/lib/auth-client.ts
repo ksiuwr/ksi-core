38a1e32f01d162136b5b40760788cd55783782c1
Michał Kosior @crqch <48185508+crqch@users.noreply.github.com>
2026-08-21 21:15:17 +0200

    fix(frontend): update auth url

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -2,6 +2,7 @@ import { createAuthClient } from 'better-auth/svelte';
 import { getUrls } from 'shared';
 
 export const authClient = createAuthClient({
-  baseURL: getUrls().AUTH
+  baseURL: getUrls().AUTH,
+  basePath: '/auth/'
   /** The base URL of the server (optional if you're using the same domain) */
 });
--- packages/shared/index.ts
+++ packages/shared/index.ts
@@ -3,12 +3,12 @@ export const getUrls = () => {
     return {
       BACKEND: 'https://api.ksi.edu.pl/',
       FRONTEND: 'https://ksi.edu.pl/',
-      AUTH: 'https://api.ksi.edu.pl/'
+      AUTH: 'https://api.ksi.edu.pl/auth/'
     };
 
   return {
     BACKEND: 'http://localhost:3000/api',
     FRONTEND: 'http://localhost:5173',
-    AUTH: 'http://localhost:3000/'
+    AUTH: 'http://localhost:3000/api/auth/'
   };
 };
