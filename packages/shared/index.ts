715c4229d5473eb911f489b7ef912441b7c53f7f
Michał Kosior @crqch <48185508+crqch@users.noreply.github.com>
2026-08-21 21:54:36 +0200

    chore: update urls

--- packages/shared/index.ts
+++ packages/shared/index.ts
@@ -1,14 +1,14 @@
 export const getUrls = () => {
   if (process.env.NODE_ENV === 'production')
     return {
-      BACKEND: 'https://api.ksi.edu.pl/',
-      FRONTEND: 'https://ksi.edu.pl/',
-      AUTH: 'https://api.ksi.edu.pl/auth/'
+      BACKEND: 'https://api.ksi.edu.pl',
+      FRONTEND: 'https://ksi.edu.pl',
+      AUTH: 'https://api.ksi.edu.pl/auth'
     };
 
   return {
     BACKEND: 'http://localhost:3000/api',
     FRONTEND: 'http://localhost:5173',
-    AUTH: 'http://localhost:3000/api/auth/'
+    AUTH: 'http://localhost:3000/api/auth'
   };
 };
